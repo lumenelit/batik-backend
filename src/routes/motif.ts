b538536cab4f2fcd597ee044a106c587c789e099
Saylendra Yasin <saylendrayasin@gmail.com>
2023-11-16 08:06:32 +0800

    added update motif

--- src/routes/motif.ts
+++ src/routes/motif.ts
@@ -6,5 +6,6 @@ const RouterMotif = express.Router();
 RouterMotif.get('/', CtrlrMotif.getAllMotif);
 RouterMotif.get('/:id', CtrlrMotif.getMotifById);
 RouterMotif.post('/', CtrlrMotif.addMotif);
+RouterMotif.patch('/:id', CtrlrMotif.updateMotif);
 
 module.exports = RouterMotif;
