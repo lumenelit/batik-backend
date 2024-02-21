54f47800683f093d8e0acbcd16b22e7f35151a7e
Saylendra Yasin <saylendrayasin@gmail.com>
2024-02-21 17:12:25 +0800

    fix bug : test

--- index.ts
+++ index.ts
@@ -1,60 +1,14 @@
-import express, { Express, Request, Response, Router } from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+const express = require('express');
 
-dotenv.config();
+const app = express();
 
-const ConnectDB = async () => {
-  const MongoURI = `${process.env.DATABASE}`;
-  return new Promise((resolve, reject) => {
-    mongoose.set('strictQuery', false);
-    mongoose
-      .connect(MongoURI)
-      .then(() => {
-        console.log('MongoDB Connected');
-        resolve(true);
-      })
-      .catch((err) => {
-        console.log(err);
-        reject(false);
-      });
-  });
-};
+app.get('/', (req, res) => {
+  res.send('Express on Vercel');
+});
 
-const cors = require('cors');
-const app: Express = express();
-const port = process.env.PORT;
-const RouterApi = Router();
+app.listen(5000, () => {
+  console.log('Running on port 5000.');
+});
 
-app.use(express.urlencoded({ limit: '30000kb', extended: true }));
-
-app.use(express.json({ limit: '30000kb' }));
-
-app.use(require('express-fileupload')());
-
-app.use(cors());
-
-app.use('/api', RouterApi);
-
-(async () => {
-  try {
-    // await ConnectDB();
-    RouterApi.get('/', (req: Request, res: Response) => {
-      res.send('Express + TypeScript Server');
-    });
-
-    RouterApi.use('/industri', require('./src/routes/industri'));
-
-    RouterApi.use('/motif', require('./src/routes/motif'));
-
-    RouterApi.use('/pesanan', require('./src/routes/pesanan'));
-
-    app.listen(port, () => {
-      console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-    });
-  } catch (error) {
-    console.error('Error starting the server:', error);
-  }
-})();
-
+// Export the Express API
 module.exports = app;
