b3863b05e0158e21a9405e06db81a5e5d6f347a9
Saylendra Yasin <saylendrayasin@gmail.com>
2024-02-21 17:53:33 +0800

    Revert "fix deployment"
    This reverts commit 0db7ba97239633460841257bc790f4107d14dad3.

--- index.ts
+++ index.ts
@@ -1,23 +1,23 @@
 import express, { Express, Request, Response, Router } from 'express';
 import dotenv from 'dotenv';
-// import mongoose from 'mongoose'; // Hapus import ini jika tidak akan menggunakan koneksi database
+import mongoose from 'mongoose';
 
 dotenv.config();
 
-// const ConnectDB = async () => {
-//   const MongoURI = `${process.env.DATABASE}`;
-//   try {
-//     await mongoose.connect(MongoURI);
-//     console.log('MongoDB Connected');
-//   } catch (err) {
-//     console.error('MongoDB Connection Error:', err);
-//     throw err;
-//   }
-// };
+const ConnectDB = async () => {
+  const MongoURI = `${process.env.DATABASE}`;
+  try {
+    await mongoose.connect(MongoURI);
+    console.log('MongoDB Connected');
+  } catch (err) {
+    console.error('MongoDB Connection Error:', err);
+    throw err;
+  }
+};
 
 const cors = require('cors');
 const app: Express = express();
-const port = process.env.PORT || 3000; // Default port 3000 if PORT is not set
+const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(express.json({ limit: '30mb' }));
@@ -28,21 +28,23 @@ const RouterApi = Router();
 
 app.use('/api', RouterApi);
 
-// Panggil ConnectDB dan tunggu koneksi sebelum menjalankan server
-// ConnectDB().then(() => { // Hapus pemanggilan ConnectDB()
-// Set up routing
-RouterApi.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+export const startServer = async () => {
+  try {
+    await ConnectDB();
+    RouterApi.get('/', (req: Request, res: Response) => {
+      res.send('Express + TypeScript Server');
+    });
 
-// RouterApi.use('/industri', require('./src/routes/industri'));
-// RouterApi.use('/motif', require('./src/routes/motif'));
-// RouterApi.use('/pesanan', require('./src/routes/pesanan'));
+    RouterApi.use('/industri', require('./src/routes/industri'));
+    RouterApi.use('/motif', require('./src/routes/motif'));
+    RouterApi.use('/pesanan', require('./src/routes/pesanan'));
 
-// Jalankan server
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
-// }).catch(err => {
-//   console.error('Error starting the server:', err);
-// });
+    app.listen(port, () => {
+      console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error('Error starting the server:', error);
+  }
+};
+
+startServer();
